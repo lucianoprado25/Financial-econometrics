58c56322ce19597dccf4b73754f187e3eb08088f
Luciano Prado <luciano.prado2511@gmail.com>
2026-04-17 10:36:57 +0200

    Create main.r

---
+++ main.r
@@ -0,0 +1,9 @@
+# Main runner 
+
+source("requirements.R")
+
+source("scripts/01_ols_capm_reset.R")
+source("scripts/02_ols_diagnostics.R")
+source("scripts/03_arma_identification.R")
+source("scripts/04_unit_root_cointegration_ecm.R")
+source("scripts/05_garch_models.R")
